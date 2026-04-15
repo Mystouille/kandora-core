085a2680e499097d7241db5fff0bff1e32751460
Myst <benoit.dessort@gmail.com>
2026-04-15 21:12:35 +0200

    team filter fix

--- Substitution.ts
+++ Substitution.ts
@@ -7,7 +7,7 @@ export const SubstitutionModelName = "Substitution";
 const substitutionSchema = new mongoose.Schema(
   {
     league: { type: ObjectId, ref: "League", required: true },
-    team: { type: ObjectId, ref: "Team", required: true },
+    team: { type: ObjectId, ref: "Team", required: false },
     replacedPlayer: { type: ObjectId, ref: "User", required: true },
     substitutePlayer: { type: ObjectId, ref: "User", required: true },
   },
